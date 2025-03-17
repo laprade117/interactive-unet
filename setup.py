af2999a0eaab25174675998384f8e178effa12a1
William Laprade <blaprade117@gmail.com>
2025-03-17 13:27:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ setup(
     entry_points = {
         'console_scripts': ['interactive-unet=interactive_unet:app'],
     },
-    install_requires=['torch>=2.6.0',
-                      'torchvision>=0.21.0',
+    install_requires=['torch',
+                      'torchvision',
                       'nicegui',
                       'scikit-image',
                       'opencv-python',
