1a0055c63cf10842028b734c0cd804a612e42349
William Laprade <blaprade117@gmail.com>
2025-06-27 13:28:19 +0200

    Update app.py

--- interactive_unet/app.py
+++ interactive_unet/app.py
@@ -824,4 +824,5 @@ with ui.dialog() as dialog, ui.card():
 randomize()
 
 # ui.run(host='0.0.0.0', port=9546, show=False, reload=False)
-ui.run(reload=True, port=9546)
+# ui.run(reload=True)
+ui.run(on_air=True)
